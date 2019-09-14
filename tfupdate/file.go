05e6b5bc5e93306eaade4fa2accf2a673c8593d0
Masayuki Morita <minamijoyo@gmail.com>
2019-09-14 22:42:28 +0900

    Move UpdateHCL to updater.go

--- tfupdate/file.go
+++ tfupdate/file.go
@@ -3,13 +3,9 @@ package tfupdate
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
-
-	"github.com/hashicorp/hcl2/hcl"
-	"github.com/hashicorp/hcl2/hclwrite"
 )
 
 // UpdateFile updates version constraints in a single file.
@@ -36,47 +32,6 @@ func UpdateFile(filename string, o Option) error {
 		}
 	} else {
 		log.Printf("[DEBUG] No changes. Skip writing file: %s", filename)
-	}
-
-	return nil
-}
-
-// UpdateHCL reads HCL from io.Reader and writes updated HCL to io.Writer.
-// Note that a filename is used only for an error message.
-func UpdateHCL(r io.Reader, w io.Writer, filename string, o Option) error {
-	src, err := ioutil.ReadAll(r)
-	if err != nil {
-		return fmt.Errorf("failed to read input: %s", err)
-	}
-
-	log.Printf("[DEBUG] Parse HCL: %s", filename)
-	f, diags := hclwrite.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
-	if diags.HasErrors() {
-		return fmt.Errorf("failed to parse input: %s", diags)
-	}
-
-	log.Printf("[DEBUG] Initialize updater: %s, %#v", filename, o)
-	u, err := NewUpdater(o)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("[DEBUG] Update HCL: %s", filename)
-	u.Update(f)
-	updated := f.BuildTokens(nil).Bytes()
-
-	// Write contents to buffer if changed.
-	if !bytes.Equal(src, updated) {
-		log.Printf("[DEBUG] Detect changes: %s", filename)
-		log.Printf("[DEBUG] Execute fmt: %s", filename)
-		result := hclwrite.Format(updated)
-
-		log.Printf("[DEBUG] Write buffer: %s", filename)
-		if _, err := w.Write(result); err != nil {
-			return fmt.Errorf("failed to write output: %s", err)
-		}
-	} else {
-		log.Printf("[DEBUG] No changes. Skip writing buffer: %s", filename)
 	}
 
 	return nil
--- tfupdate/updater.go
+++ tfupdate/updater.go
@@ -1,8 +1,14 @@
 package tfupdate
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"strings"
 
+	"github.com/hashicorp/hcl2/hcl"
 	"github.com/hashicorp/hcl2/hclwrite"
 	"github.com/pkg/errors"
 )
@@ -48,4 +54,45 @@ func NewOption(updateType string, target string) Option {
 		updateType: updateType,
 		target:     target,
 	}
+}
+
+// UpdateHCL reads HCL from io.Reader and writes updated HCL to io.Writer.
+// Note that a filename is used only for an error message.
+func UpdateHCL(r io.Reader, w io.Writer, filename string, o Option) error {
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("failed to read input: %s", err)
+	}
+
+	log.Printf("[DEBUG] Parse HCL: %s", filename)
+	f, diags := hclwrite.ParseConfig(src, filename, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return fmt.Errorf("failed to parse input: %s", diags)
+	}
+
+	log.Printf("[DEBUG] Initialize updater: %s, %#v", filename, o)
+	u, err := NewUpdater(o)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Update HCL: %s", filename)
+	u.Update(f)
+	updated := f.BuildTokens(nil).Bytes()
+
+	// Write contents to buffer if changed.
+	if !bytes.Equal(src, updated) {
+		log.Printf("[DEBUG] Detect changes: %s", filename)
+		log.Printf("[DEBUG] Execute fmt: %s", filename)
+		result := hclwrite.Format(updated)
+
+		log.Printf("[DEBUG] Write buffer: %s", filename)
+		if _, err := w.Write(result); err != nil {
+			return fmt.Errorf("failed to write output: %s", err)
+		}
+	} else {
+		log.Printf("[DEBUG] No changes. Skip writing buffer: %s", filename)
+	}
+
+	return nil
 }
