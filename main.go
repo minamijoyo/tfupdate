bebf8bb6df6a40fc033adb83920601afd00e3667
Masayuki Morita <minamijoyo@gmail.com>
2021-11-12 15:57:16 +0900

    Bump version to v0.6.3

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version is a version number.
-var version = "0.6.3-beta1"
+var version = "0.6.3"
 
 // UI is a user interface which is a global variable for mocking.
 var UI cli.Ui
