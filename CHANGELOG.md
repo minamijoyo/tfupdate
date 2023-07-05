a9ab7c955c5fa00cf723fe27d01dcb9b1486fbeb
Masayuki Morita <minamijoyo@gmail.com>
2023-07-05 15:30:40 +0900

    Bump version to v0.7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## master (Unreleased)
 
+## 0.7.1 (2023/07/05)
+
 BUG FIXES:
 
 * Fix a regression issue for using absolute path ([#94](https://github.com/minamijoyo/tfupdate/pull/94))
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version is a version number.
-var version = "0.7.0"
+var version = "0.7.1"
 
 // UI is a user interface which is a global variable for mocking.
 var UI cli.Ui
