48282ea20d09c4d7050bc01f1e349d0657303c26
Masayuki Morita <minamijoyo@gmail.com>
2026-06-29 22:08:40 +0900

    Add test for fetchProviderPackageMetadata

--- lock/index_test.go
+++ lock/index_test.go
@@ -314,6 +314,162 @@ func TestBuildProviderVersionFromDownload(t *testing.T) {
 	}
 }
 
+func TestProviderIndexFetchProviderPackageMetadata(t *testing.T) {
+	allPlatforms := []string{"darwin_arm64", "darwin_amd64", "linux_amd64", "windows_amd64"}
+
+	cases := []struct {
+		desc     string
+		address  string
+		version  string
+		platform string
+		code     int
+		res      *tfregistry.ProviderPackageMetadataResponse
+		want     *ProviderVersion
+		ok       bool
+	}{
+		{
+			desc:     "simple",
+			address:  "minamijoyo/dummy",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			code:     200,
+			res:      newMockProviderPackageMetadataResponse(),
+			want: &ProviderVersion{
+				address:   "minamijoyo/dummy",
+				version:   "3.2.1",
+				platforms: allPlatforms,
+				h1Hashes: map[string]string{
+					"terraform-provider-dummy_3.2.1_darwin_arm64.zip":  "h1:3323G20HW9PA9ONrL6CdQCdCFe6y94kXeOTprq+Zu+w=",
+					"terraform-provider-dummy_3.2.1_darwin_amd64.zip":  "h1:63My0EuWIYHWVwWOxmxWwgrfx+58Tz+nTduelaCCAfs=",
+					"terraform-provider-dummy_3.2.1_linux_amd64.zip":   "h1:2zotrPRAjGZZMkjJGBGLnIbG+sqhQN30sbwqSDECQFQ=",
+					"terraform-provider-dummy_3.2.1_windows_amd64.zip": "h1:PwmSfP1Tb8io64qqCx9AExzIqnHiZ/ER2l8qVhEEKdw=",
+				},
+				zhHashes: map[string]string{
+					"terraform-provider-dummy_3.2.1_darwin_arm64.zip":  "zh:5622a0fd03420ed1fa83a1a6e90b65fbe34bc74c251b3b47048f14217e93b086",
+					"terraform-provider-dummy_3.2.1_darwin_amd64.zip":  "zh:fc5bbdd0a1bd6715b9afddf3aba6acc494425d77015c19579b9a9fa950e532b2",
+					"terraform-provider-dummy_3.2.1_linux_amd64.zip":   "zh:c5f0a44e3a3795cb3ee0abb0076097c738294c241f74c145dfb50f2b9fd71fd2",
+					"terraform-provider-dummy_3.2.1_windows_amd64.zip": "zh:8b75ff41191a7fe6c5d9129ed19a01eacde5a3797b48b738eefa21f5330c081e",
+				},
+			},
+			ok: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			client := &mockTFRegistryClient{
+				metadataRes: tc.res,
+				err:         nil,
+			}
+			pi := newProviderIndex(tc.address, client, nil)
+
+			got, err := pi.fetchProviderPackageMetadata(context.Background(), tc.version, tc.platform)
+
+			if tc.ok && err != nil {
+				t.Fatalf("failed to call fetchProviderPackageMetadata: err = %s", err)
+			}
+
+			if !tc.ok && err == nil {
+				t.Fatalf("expected to fail, but success: got = %s", spew.Sdump(got))
+			}
+
+			if diff := cmp.Diff(got, tc.want, cmp.AllowUnexported(ProviderVersion{})); diff != "" {
+				t.Errorf("got: %s, want = %s, diff = %s", spew.Sdump(got), spew.Sdump(tc.want), diff)
+			}
+		})
+	}
+}
+
+func TestNewProviderPackageMetadataRequest(t *testing.T) {
+	cases := []struct {
+		desc     string
+		address  string
+		version  string
+		platform string
+		want     *tfregistry.ProviderPackageMetadataRequest
+		ok       bool
+	}{
+		{
+			desc:     "simple",
+			address:  "minamijoyo/dummy",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			want: &tfregistry.ProviderPackageMetadataRequest{
+				Namespace: "minamijoyo",
+				Type:      "dummy",
+				Version:   "3.2.1",
+				OS:        "darwin",
+				Arch:      "arm64",
+			},
+			ok: true,
+		},
+		{
+			desc:     "fully qualified provider address",
+			address:  "registry.terraform.io/minamijoyo/dummy",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			want: &tfregistry.ProviderPackageMetadataRequest{
+				Namespace: "minamijoyo",
+				Type:      "dummy",
+				Version:   "3.2.1",
+				OS:        "darwin",
+				Arch:      "arm64",
+			},
+			ok: true,
+		},
+		{
+			desc:     "unknown provider namespace",
+			address:  "null",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			want:     nil,
+			ok:       false,
+		},
+		{
+			desc:     "legacy provider namespace",
+			address:  "-/null",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			want:     nil,
+			ok:       false,
+		},
+		{
+			desc:     "zero provider namespace",
+			address:  "",
+			version:  "3.2.1",
+			platform: "darwin_arm64",
+			want:     nil,
+			ok:       false,
+		},
+		{
+			desc:     "invalid platform",
+			address:  "minamijoyo/dummy",
+			version:  "3.2.1",
+			platform: "foo",
+			want:     nil,
+			ok:       false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := newProviderPackageMetadataRequest(tc.address, tc.version, tc.platform)
+
+			if tc.ok && err != nil {
+				t.Fatalf("failed to call newProviderDownloadRequest: err = %s", err)
+			}
+
+			if !tc.ok && err == nil {
+				t.Fatalf("expected to fail, but success: got = %s", spew.Sdump(got))
+			}
+
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Errorf("got: %s, want = %s, diff = %s", spew.Sdump(got), spew.Sdump(tc.want), diff)
+			}
+		})
+	}
+}
+
 func TestBuildProviderVersionFromPackageMetadata(t *testing.T) {
 	allPlatforms := []string{"darwin_arm64", "darwin_amd64", "linux_amd64", "windows_amd64"}
 
@@ -329,38 +485,7 @@ func TestBuildProviderVersionFromPackageMetadata(t *testing.T) {
 			desc:    "simple",
 			address: "minamijoyo/dummy",
 			version: "3.2.1",
-			res: &tfregistry.ProviderPackageMetadataResponse{
-				Filename:    "terraform-provider-dummy_3.2.1_darwin_arm64.zip",
-				DownloadURL: "https://github.com/opentofu/terraform-provider-dummy/releases/download/v3.2.1/terraform-provider-dummy_3.2.1_darwin_arm64.zip",
-				SHASum:      "",
-				SHASumsURL:  "https://github.com/opentofu/terraform-provider-dummy/releases/download/v3.2.1/terraform-provider-dummy_3.2.1_SHA256SUMS",
-				Packages: map[string]tfregistry.Package{
-					"darwin_arm64": {
-						Hashes: []string{
-							"zh:5622a0fd03420ed1fa83a1a6e90b65fbe34bc74c251b3b47048f14217e93b086",
-							"h1:3323G20HW9PA9ONrL6CdQCdCFe6y94kXeOTprq+Zu+w=",
-						},
-					},
-					"darwin_amd64": {
-						Hashes: []string{
-							"zh:fc5bbdd0a1bd6715b9afddf3aba6acc494425d77015c19579b9a9fa950e532b2",
-							"h1:63My0EuWIYHWVwWOxmxWwgrfx+58Tz+nTduelaCCAfs=",
-						},
-					},
-					"linux_amd64": {
-						Hashes: []string{
-							"zh:c5f0a44e3a3795cb3ee0abb0076097c738294c241f74c145dfb50f2b9fd71fd2",
-							"h1:2zotrPRAjGZZMkjJGBGLnIbG+sqhQN30sbwqSDECQFQ=",
-						},
-					},
-					"windows_amd64": {
-						Hashes: []string{
-							"zh:8b75ff41191a7fe6c5d9129ed19a01eacde5a3797b48b738eefa21f5330c081e",
-							"h1:PwmSfP1Tb8io64qqCx9AExzIqnHiZ/ER2l8qVhEEKdw=",
-						},
-					},
-				},
-			},
+			res:     newMockProviderPackageMetadataResponse(),
 			want: &ProviderVersion{
 				address:   "minamijoyo/dummy",
 				version:   "3.2.1",
